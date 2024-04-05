aee5bf3a71515c2a1892d936eb542fac10558275
jose manuel jaramillo valencia <josemanuelvalenciavelez@gmail.com>
2024-04-05 10:39:32 -0500

    first upload

---
+++ Prolog Taller 1/JaramilloJose-000541491-BD3.pl
@@ -0,0 +1,16 @@
+capital(colombia, bogota).
+capital(ecuador, quito).
+capital(argentina, 'buenos aires').
+capital('corea  del sur', seul).
+capital('estados unidos', 'washington dc').
+topicosYoutuber("ElRubius", "videojuegos").
+topicosYoutuber("ElRubius", "minecraft").
+topicosYoutuber("Vegetta777", "videojuegos").
+topicosYoutuber("Vegetta777", "minecraft").
+topicosYoutuber("HolaSoyGerman", "humor").
+topicosYoutuber("HolaSoyGerman", "vlogs").
+topicosYoutuber("Luisito Comunica", "viajes").
+topicosYoutuber("Luisito Comunica", "exploracion").
+topicosYoutuber("Zazza el italiano", "viajes").
+topicosYoutuber("Zazza el italiano", "documentacion").
+
